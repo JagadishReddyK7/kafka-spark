c8e50b48bb07b0cff60f5af5a9c02f59c2509f64
Jagadish Kollu <99238944+JagadishReddyK7@users.noreply.github.com>
2023-10-03 13:38:12 +0530

    Update kafka+struct_stream.py

--- kafka+struct_stream.py
+++ kafka+struct_stream.py
@@ -19,8 +19,8 @@ spark = SparkSession.builder.config('spark.master','local').\
 sc=spark.sparkContext
 
 sc._jsc.hadoopConfiguration().set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
-sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", "AKIA3JCX6CPUE24DUXOA")
-sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "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")
+sc._jsc.hadoopConfiguration().set("fs.s3a.access.key", "")
+sc._jsc.hadoopConfiguration().set("fs.s3a.secret.key", "")
 
 
 json_schema = StructType([StructField("TransactionID", StringType(), True),
