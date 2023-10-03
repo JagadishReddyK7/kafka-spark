3c96b1fef718f707b62ab8beedf9f7d824445acc
Jagadish Kollu <99238944+JagadishReddyK7@users.noreply.github.com>
2023-10-03 13:35:29 +0530

    Update s3data_down.py

--- s3data_down.py
+++ s3data_down.py
@@ -1,7 +1,7 @@
 import boto3
 import json
 
-s3 = boto3.client('s3',aws_access_key_id='AKIAZ7A2D7Q6BQDZDGYR',aws_secret_access_key='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')
+s3 = boto3.client('s3',aws_access_key_id='',aws_secret_access_key='')
 response=s3.get_object(Bucket='kafka-s3-connection-bucket',Key='random_transactions_data.csv')
 object_content=response['Body'].read().decode('utf-8')
 
